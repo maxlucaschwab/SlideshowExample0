ae49e58e3175835e23baa9981eb1d274527bb07d
maxlucaschwab <114507212+maxlucaschwab@users.noreply.github.com>
2023-04-28 18:27:02 -0700

    logo slide in

--- script.js
+++ script.js
@@ -26,24 +26,24 @@ class scene_1 extends Phaser.Scene {
             targets: this.imageObject,
             alpha: 1,
             duration: 2000,
-        })
+        });
 
         // progress to next scene 
         let space = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
         space.on('down', () => {
             this.scene.start("scene_2");
-        })
+        });
 
         // fade to black
         this.time.delayedCall(4000, () => {
             this.cameras.main.fadeOut(2000, 0, 0, 0)
-        })
+        });
     
         this.cameras.main.once(Phaser.Cameras.Scene2D.Events.FADE_OUT_COMPLETE, (cam, effect) => {
             this.time.delayedCall(500, () => {
                 this.scene.start('scene_2')
-            })
-        })
+            });
+        });
 
     }
 
@@ -103,7 +103,7 @@ class scene_2 extends Phaser.Scene {
                 targets: this.textObject,
                 alpha: 1,
                 duration: 1000,
-            })
+            });
 
             this.textObject = this.add.text(
                 800,
@@ -122,7 +122,7 @@ class scene_2 extends Phaser.Scene {
                     targets: this.textObject,
                     alpha: 1,
                     duration: 1000,
-                })
+                });
             });
         });
         // End text rendering
@@ -131,24 +131,24 @@ class scene_2 extends Phaser.Scene {
         let space = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
         space.on('down', () => {
             this.scene.start("scene_3");
-        })
+        });
 
         // progress to previous scene
         let left = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
         left.on('down', () => {
             this.scene.start("scene_1");
-        })
+        });
 
         // fade to black
         this.time.delayedCall(7000, () => {
-            this.cameras.main.fadeOut(2000, 0, 0, 0)
-        })
+            this.cameras.main.fadeOut(2000, 0, 0, 0);
+        });
     
         this.cameras.main.once(Phaser.Cameras.Scene2D.Events.FADE_OUT_COMPLETE, (cam, effect) => {
             this.time.delayedCall(500, () => {
                 this.scene.start('scene_3')
-            })
-        })
+            });
+        });
 
     }
 
@@ -175,7 +175,7 @@ class scene_3 extends Phaser.Scene {
             this.cameras.main.centerX,
             this.cameras.main.centerY,
             "3_background",
-        )
+        );
         
         this.imageObject.setScale(0.5);
 
@@ -183,10 +183,11 @@ class scene_3 extends Phaser.Scene {
         this.textObject = this.add.text(
             150,
             200,
-            "A challenger approaches…",
+            "A challenger\n" + "approaches…",
             {
-                font: "30px bold",
-                color: "#ffffff"
+                font: "40px bold",
+                color: "#ffffff",
+                align: "center"
             }
         );
         
@@ -198,15 +199,16 @@ class scene_3 extends Phaser.Scene {
                 targets: this.textObject,
                 alpha: 1,
                 duration: 1000,
-            })
+            });
 
             this.textObject = this.add.text(
                 800,
                 350,
-                "One who seeks to become WHOLE…",
+                "One who seeks to become\n" + "WHOLE…",
                 {
-                    font: "30px bold",
-                    color: "#ffffff"
+                    font: "40px bold",
+                    color: "#ffffff",
+                    align: "center"
                 }
             );
     
@@ -233,6 +235,17 @@ class scene_3 extends Phaser.Scene {
         left.on('down', () => {
             this.scene.start("scene_2");
         });
+
+        // fade to black
+        this.time.delayedCall(7000, () => {
+            this.cameras.main.fadeOut(2000, 0, 0, 0)
+        });
+    
+        this.cameras.main.once(Phaser.Cameras.Scene2D.Events.FADE_OUT_COMPLETE, (cam, effect) => {
+            this.time.delayedCall(500, () => {
+                this.scene.start('scene_4')
+            })
+        });
     }
 
     update(){}
@@ -248,25 +261,55 @@ class scene_4 extends Phaser.Scene {
         this.load.path = "./assets/";
         this.load.image("4_background", "scene4_bg.png");
         this.load.image("4_foreground", "scene4_fg.png");
+        this.load.image("title", "VoidKnightLogo.png");
         console.log("Finished preloading!");
     }
 
     create(){
-        this.imageObject = this.add.image(
-            this.cameras.main.centerX,
-            this.cameras.main.centerY,
-            "4_background",
-        )
-        
-        this.imageObject.setScale(0.5);
+
+        this.cameras.main.fadeIn(1000, 0, 0, 0)
 
         this.imageObject = this.add.image(
             this.cameras.main.centerX,
             this.cameras.main.centerY,
-            "4_foreground",
-        )
+            "4_background",
+        );
         
         this.imageObject.setScale(0.5);
+
+        this.imageObject = this.add.image(
+            1350,
+            this.cameras.main.centerY,
+            "4_foreground",
+        );
+
+        this.tweens.add({
+            targets: this.imageObject,
+            x: this.cameras.main.centerX,
+            duration: 1500,
+            ease: "bezier"
+        });
+
+        this.imageObject.setScale(0.5);
+
+        this.graphics = this.add.graphics();
+        this.graphics.fillStyle(0xffeb86, 1);
+        this.graphics.fillCircle(297, 205, 28);
+
+        this.imageObject = this.add.image(
+            -300,
+            200,
+            "title"
+        );
+
+        this.tweens.add({
+            targets: this.imageObject,
+            x: 375,
+            duration: 1500,
+            ease: "bezier"
+        });
+
+        this.imageObject.setScale(0.8);
     
         // progress to next scene
         // let space = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
@@ -278,7 +321,7 @@ class scene_4 extends Phaser.Scene {
         let left = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
         left.on('down', () => {
             this.scene.start("scene_3");
-        })
+        });
     }
 
     update(){}
